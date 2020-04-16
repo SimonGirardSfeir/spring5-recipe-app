30b6f6a1e6c99fc8c7436f4d1fcb7736ed88e6c5
SimonGirardSfeir <girard.s@sfeir.com>
2020-04-16 17:33:30 +0200

    Recipe Controller Number Format Exception

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -55,14 +55,18 @@ public class RecipeControllerTest {
 
     @Test
     public void getRecipeNotFound() throws Exception {
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
-
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
+    }
+
+    @Test
+    public void getRecipeNumberFormatException() throws Exception {
+        mockMvc.perform(get("/recipe/abc/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
     @Test
