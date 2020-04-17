23961696227fe5afeddca6fb19602ac8005e066e
SimonGirardSfeir <girard.s@sfeir.com>
2020-04-17 15:16:36 +0200

    Data Validation Recipe

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -89,11 +89,24 @@ public class RecipeControllerTest {
 
         mockMvc.perform(post("/recipe").contentType(MediaType.APPLICATION_FORM_URLENCODED)
                     .param("id","")
-                    .param("description", "some strings"))
+                    .param("description", "some strings")
+                    .param("directions", "some directions"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
 
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id",""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipeform"));
+    }
     @Test
     public void testGetUpdateView() throws Exception {
         RecipeCommand command = new RecipeCommand();
